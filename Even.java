b6ab929e504b271203df0c76659435a441f15a97
Krishnaa-M <krishnaa1513@gmail.com>
2017-07-05 12:14:35 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -1,1 +1,15 @@
-
+import java.io.*;
+public class Even {
+	public static void main(String [] args) throws IOException{
+		BufferedReader a=new BufferedReader(new InputStreamReader(System.in));
+		Integer b=Integer.parseInt(a.readLine());
+		if(b%2==0)
+		{
+			System.out.println("Even");
+		}
+		if(b%2==1)
+		{
+			System.out.println("Odd");
+		}
+	}
+}
