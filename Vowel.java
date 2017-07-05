b28dbd501f503ed95afe30001c69ffe804d3a50b
Krishnaa-M <krishnaa1513@gmail.com>
2017-07-05 14:40:45 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Vowel {
+	public static void main(String [] args) throws IOException{
+		BufferedReader a=new BufferedReader(new InputStreamReader(System.in));
+		char b=a.readLine().charAt(0);
+		if(b=='A' || b=='a' || b=='E' || b=='e' || b=='I' || b=='i' || b=='o' || b=='O' || b=='U' || b=='u')
+		{
+			System.out.println("Vowel");
+		}
+		else
+		{
+			System.out.println("Consonant");
+		}
+	}
+}
