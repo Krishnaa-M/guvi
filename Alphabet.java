62044035eeb91709f9a8e810d5780daa9feb0825
Krishnaa-M <krishnaa1513@gmail.com>
2017-07-05 15:08:57 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Alphabet{
+	public static void main(String [] args) throws IOException{
+		BufferedReader a=new BufferedReader(new InputStreamReader(System.in));
+		char b=a.readLine().charAt(0);
+		if((b>='a' && b<='z') || (b>='A' && b<='Z'))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			System.out.println("Not an Alphabet");
+		}
+	}
+}
