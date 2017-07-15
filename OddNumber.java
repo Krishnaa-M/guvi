e82c776c4546325136c073f8774425c5585ef4cf
Krishnaa-M <krishnaa1513@gmail.com>
2017-07-15 10:55:43 +0530

    Create OddNumber.java

---
+++ OddNumber.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class OddNumber {
+public static void main(String[] args) throws NumberFormatException, IOException{
+	BufferedReader a=new BufferedReader(new InputStreamReader(System.in));
+	Integer b= Integer.parseInt(a.readLine());
+	Integer c= Integer.parseInt(a.readLine());
+	for(int i=b;i<c;i++)
+	{
+	  		if(i%2!=0)
+	  		{
+	  			System.out.println(i);
+	  		}
+	}
+}
+}
