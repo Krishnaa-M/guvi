5cac6dd3c9a0d367d32433f8207cd07d5083ac39
Krishnaa-M <krishnaa1513@gmail.com>
2017-07-19 14:35:20 +0530

    Update StringtoInteger.java

--- StringtoInteger.java
+++ StringtoInteger.java
@@ -1,1 +1,10 @@
-
+import java.io.*;
+public class StringtoInteger {
+public static void main(String[] args) throws IOException{
+	BufferedReader a= new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter the String");
+	String s=a.readLine();
+	int b=Integer.parseInt(s);
+	System.out.println(b);
+}
+}
